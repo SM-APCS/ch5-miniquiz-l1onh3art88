410fdf6d0a9ada4bcee1c962468f716cf62058ff
Kurt <Kurt@10.0.1.14>
2015-11-05 10:32:33 -0600

    Added Print out line

---
+++ Ch5-MiniQuiz/src/ch5/miniquiz/Ch5MiniQuiz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch5.miniquiz;
+
+/**
+ *
+ * @author Kurt
+ */
+public class Ch5MiniQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
