dcb445b7ae05cf78f964ec50d05172a1be298823
l1onh3art88 <spenccheng@gmail.com>
2015-11-06 11:26:14 -0600

    Ch5Miniquiz

---
+++ Ch5-MiniQuiz/src/ch5/miniquiz/Complexity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch5.miniquiz;
+
+/**
+ *
+ * @author Spencer Cheng
+ * 
+ * Represents the interface for an object that can be assigned an 
+ * explicit complexity
+ */
+public interface Complexity {
+    public void setComplexity (int complexity);
+    public int getComplexity();
+}
