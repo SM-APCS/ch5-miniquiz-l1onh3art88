dd3501f15fbf43e4eaa89a66fed6f4e64834fb7c
l1onh3art88 <spenccheng@gmail.com>
2015-11-06 11:25:50 -0600

    Ch5Miniquiz

---
+++ Ch5-MiniQuiz/src/ch5/miniquiz/ParameterTester.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch5.miniquiz;
+
+/**
+ *
+ * @author Spencer Cheng
+ * 
+ * ParameterTester.java Author: Lewis/Loftus/Cocking
+ * 
+ * Demonstrates the effects of passing various types of parameters.
+ */
+public class ParameterTester {
+    //-------------------------------------------------
+    // Modifies the parameters, printing their values before and 
+    // after making the changes.
+    //------------------------------------------------
+    public void changeValues (int f1, Num f2, Num f3){
+        System.out.println("Before changing the values:");
+        System.out.println("f1\tf2\f3");
+        System.out.println(f1 + "\t" + f2 + "\t" + f3 +"\n");
+        
+        f1=999;
+        f2.setValue(888);
+        f3= new Num(777);
+        
+        System.out.println("After changing the values:");
+        System.out.println("f1\tf2\f3");
+        System.out.println(f1 + "\t" + f2 + "\t" + f3+"\n");
+        
+    }
+            
+}
